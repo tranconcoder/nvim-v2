64547e19b0c00cd156b17e94cf9d070953f6f89d
tvconss <tranvanconkg@gmail.com>
2026-03-26 10:52:07 +0700

    feat: auto-save on focus lost and buffer leave

--- lua/modules/autocmds.lua
+++ lua/modules/autocmds.lua
@@ -1,3 +1,12 @@
+vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
+  group = vim.api.nvim_create_augroup("auto-save", { clear = true }),
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+      vim.cmd("silent! write")
+    end
+  end,
+})
+
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
